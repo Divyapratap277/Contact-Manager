1c9047820c9a81326e2869553aa978ffb5f9a76a
Divyapratapsingh Chaudhary <Divyapratap68@gmail.com>
2025-10-02 09:54:25 +0530

    backend part2

--- MyContact_backend/controller/contactController.js
+++ MyContact_backend/controller/contactController.js
@@ -1,3 +1,55 @@
+const asyncHandler= require('express-async-handler')
+
+
 //@desc get all contact
 //@route GET /api/contact
 //@access public 
+
+const getContacts = asyncHandler(async (req,res)=>{
+   res.status(200).send({message:"Get all contacts"});
+})
+
+
+//@desc Create  contact
+//@route POST /api/contact
+//@access public 
+
+const createContact = asyncHandler (async (req,res)=>{
+    console.log("The req body is " ,req.body);
+    const{name,age,city} = req.body;
+    if(!name || !age || !city)
+    {
+        res.status(400);
+        throw new Error("All fields are mandatory")
+    }
+   res.status(201).send({message: "Create contacts"});
+})
+
+//@desc get  contact
+//@route GET /api/contact/:id
+//@access public 
+
+const getContact =asyncHandler  (async (req,res)=>{
+   res.status(200).send({message : `Get contact for ${req.params.id}`});
+})
+
+//@desc update contact
+//@route PUT /api/contact/:id
+//@access public 
+
+const updateContact =asyncHandler (async (req,res)=>{
+   res.status(200).send({mesaage: `Update contact for ${req.params.id}`});
+})
+
+
+//@desc Delete contact
+//@route delete /api/contact/:id
+//@access public 
+
+const deleteContact =asyncHandler (async (req,res)=>{
+   res.status(200).send({message: `Delete contact of ${req.params.id}`});
+})
+
+
+
+module.exports= {getContacts, createContact, getContact, updateContact, deleteContact };
--- MyContact_backend/routes/contactRoutes.js
+++ MyContact_backend/routes/contactRoutes.js
@@ -1,24 +1,15 @@
 const express= require('express')
 const router= express.Router();
+const {getContacts, createContact, getContact, updateContact, deleteContact} = require('../controller/contactController')
 
-router.route("/").get((req,res)=>{
-   res.status(200).send({message:"Get all contacts"});
-})
+router.route("/").get(getContacts)
 
-router.route("/").post((req,res)=>{
-   res.status(200).send({message: "Create contacts"});
-})
+router.route("/").post(createContact)
 
-router.route("/:id").get((req,res)=>{
-   res.status(200).send({message : `Get contact for ${req.params.id}`});
-})
+router.route("/:id").get(getContact)
 
-router.route("/:id").put((req,res)=>{
-   res.status(200).send({mesaage: `Update contact for ${req.params.id}`});
-})
+router.route("/:id").put(updateContact)
 
-router.route("/:id").delete((req,res)=>{
-   res.status(200).send({message: `Delete contact of ${req.params.id}`});
-})
+router.route("/:id").delete(deleteContact)
 
 module.exports= router;
